f14c912f58d4ee9f39334afb170e7e5d92c1a9c1
isabellaccosta <isabellacosta.rdg@gmail.com>
2026-01-14 16:10:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -31,3 +31,9 @@ const observer = new IntersectionObserver(entries=>{
 },{threshold:0.2});
 
 faders.forEach(el=>observer.observe(el));
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.navbar nav');
+
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
